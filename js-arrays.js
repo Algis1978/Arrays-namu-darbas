2980771e90c6708db24b8a760649e8cd17d7e55e
AlgirdasNavickas <algirdas.navickas@yahoo.com>
2021-08-12 08:26:10 +0300

    Paruošiamasis html

---
+++ js-arrays.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        name: 'Jonas',
+        marks: [10, 2, 8, 4, 6]
+    },
+    {
+        name: 'Maryte',
+        marks: [3, 7, 9, 5]
+    },
+    {
+        name: 'Petras',
+        marks: [7, 7, 7]
+    },
+    {
+        name: 'Ona',
+        marks: [10, 9, 8, 7, 6]
+    },
+]
+
+
+// Išspausdinti mokinio vardą ir pažymį, kuris turi prasčiausią pažymį.
+// Išspausdinti mokinio vardą ir pažymį, kuris turi geriausią pažymį.
+// Išspausdinti mokinio vardą ir pažymiu vidurkį, kuris turi prasciausią pažymių vidurkį.
+// Išspausdinti mokinio vardą ir pažymiu vidurkį, kuris turi geriausią pažymių vidurkį.
+// Išspausdinti mokinio vardą, jei jis turi pažymį 8.
+// Išspausdinti mokinio vardą, jei jis neturi pažymio 7.
+// PASTABA: jei kazkurį reikalavimą atitinka daugiau nei vienas mokinys, spausdinti visus.
+document.write ("<h3>UŽDUOTYS</h3><p>Pagal duotą duomenų masyvą:<br>1. Išspausdinti mokinio vardą ir pažymį, kuris turi prasčiausią pažymį.<br>2. Išspausdinti mokinio vardą ir pažymį, kuris turi geriausią pažymį.<br>3. Išspausdinti mokinio vardą ir pažymiu vidurkį, kuris turi prasciausią pažymių vidurkį.<br>4. Išspausdinti mokinio vardą ir pažymiu vidurkį, kuris turi geriausią pažymių vidurkį.<br>5. Išspausdinti mokinio vardą, jei jis turi pažymį 8.<br>6. Išspausdinti mokinio vardą, jei jis neturi pažymio 7.<br><br>PASTABA: jei kazkurį reikalavimą atitinka daugiau nei vienas mokinys, spausdinti visus.<h3>DUOMENYS</h3><p>Duomenų masyvas:</p><h3>JAVASCRIPT PROGRAMOS REZULTATAS: </h3>");
